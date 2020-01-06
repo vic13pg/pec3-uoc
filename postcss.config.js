ea91cd0c9b64cd34e852b3f5e9704a27468c38a1
vic13pg <vperisg@gmail.com>
2020-01-06 19:58:30 +0100

    5.0.0

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: [
+    require("postcss-import"),
+   require("tailwindcss")("./tailwind.config.js"),
+    require("autoprefixer"),
+    require("postcss-clean")(),
+    require("postcss-preset-env")({
+      stage: 3,
+      features: {
+        "nesting-rules": true,
+        "case-insensitive-attributes": true,
+        "hexadecimal-alpha-notation": true,
+        "place-properties": true
+      }
+    })
+  ]
+};
